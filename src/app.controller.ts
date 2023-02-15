3eb531cd0313cbbce2a224e62c1b087374c04bc6
IDR6IX DUVAL <idrissleutch@cinaf.tv>
2023-02-15 12:22:51 +0100

    refact infra

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,18 +9,19 @@ import {
   Res,
   UploadedFile,
   UseInterceptors,
-  HttpException, StreamableFile
+  HttpException,
+  StreamableFile,
 } from '@nestjs/common';
 import { AppService } from './app.service';
-import {FileInterceptor} from "@nestjs/platform-express";
-import {diskStorage} from "multer";
-import {generateUniqueFileName} from "./common/functions/filename";
-import path, {join} from "path";
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { generateUniqueFileName } from './common/functions/filename';
+import path, { join } from 'path';
 import mine = require('mime-types');
-import {NextFunction} from "express";
-import * as fs from "fs";
-import {createReadStream} from "fs";
-import env from "./common/constants/settings";
+import { NextFunction } from 'express';
+import * as fs from 'fs';
+import { createReadStream } from 'fs';
+import env from './common/constants/settings';
 
 @Controller()
 export class AppController {
@@ -32,57 +33,55 @@ export class AppController {
   }
 
   @Post('storage/file')
-  @UseInterceptors(FileInterceptor('file', {
-    storage: diskStorage({
-      destination: env.STORAGE_DIR,
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: env.STORAGE_DIR,
 
-      filename(req, file, callback) {
-        callback(null, generateUniqueFileName(file.originalname));
-      },
+        filename(req, file, callback) {
+          callback(null, generateUniqueFileName(file.originalname));
+        },
+      }),
     }),
-  }))
+  )
   uploadFile(@UploadedFile() file: Express.Multer.File) {
-    if(file){
+    if (file) {
       return {
-        file: file.filename
-      }
-    }else {
-      throw new HttpException({
-        message: 'Vous devez choisir un fichier'
-      }, HttpStatus.FORBIDDEN)
+        file: file.filename,
+      };
+    } else {
+      throw new HttpException(
+        {
+          message: 'Vous devez choisir un fichier',
+        },
+        HttpStatus.FORBIDDEN,
+      );
     }
   }
 
-
   @Get('storage/:filePath')
   async handleStreamFile(
-      @Param() param: {filePath: string},
-      @Res({ passthrough: true }) res,
-      @Ip() ip,
-      @Next() next: NextFunction,
+    @Param() param: { filePath: string },
+    @Res({ passthrough: true }) res,
   ) {
-    const UPLOAD_FOLDER = env.STORAGE_DIR
+    const UPLOAD_FOLDER = env.STORAGE_DIR;
 
-    let rootToFile = join(process.cwd(), UPLOAD_FOLDER, param.filePath);
+    const rootToFile = join(process.cwd(), UPLOAD_FOLDER, param.filePath);
 
     if (!fs.existsSync(rootToFile)) {
       throw new HttpException(
-          {
-            status: HttpStatus.NOT_FOUND,
-            message: 'The file was trying to get not found',
-          },
-          HttpStatus.NOT_FOUND,
+        {
+          status: HttpStatus.NOT_FOUND,
+          message: 'The file was trying to get not found',
+        },
+        HttpStatus.NOT_FOUND,
       );
     }
 
-
-      res.set({
-        'Content-Type': mine.contentType(rootToFile),
-      });
-      const streamable = createReadStream(rootToFile);
-      return new StreamableFile(streamable);
-    }
-
-
+    res.set({
+      'Content-Type': mine.contentType(rootToFile),
+    });
+    const streamable = createReadStream(rootToFile);
+    return new StreamableFile(streamable);
+  }
 }
-
--- src/common/constants/settings.ts
+++ src/common/constants/settings.ts
@@ -1,12 +1,12 @@
-import {load} from "ts-dotenv"
+import { load } from 'ts-dotenv';
 
 const env = load({
-    APP_NAME: String,
-    PROD: Boolean,
-    PORT: Number,
-    DB: String,
-    DB_TEST: String,
-    STORAGE_DIR: String
-})
+  APP_NAME: String,
+  PROD: Boolean,
+  PORT: Number,
+  DB: String,
+  DB_TEST: String,
+  STORAGE_DIR: String,
+});
 
-export default env
+export default env;
